9a44a688c9d14d5e50d05d5287af31b15f497fdb
Jeff <jmartinbaldwin@gmail.com>
2026-01-14 15:56:19 +0900

    move setup responsibility into Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,11 @@
+require_relative 'board'
+require_relative 'player'
+
+
 class Game
   def initialize
-    
+    @board = Board.new
+    @player_one = Player.new
+    @player_two = Player.new
   end
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,3 @@
-require_relative 'lib/board'
 require_relative 'lib/game'
-require_relative 'lib/player'
 
+game = Game.new
