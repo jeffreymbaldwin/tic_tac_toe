5c32e6241236ddd7652cdb7b91cb23db3e0424e6
Jeff <jmartinbaldwin@gmail.com>
2026-01-22 14:18:24 +0900

    add function for human to input selection on the game board

--- lib/game.rb
+++ lib/game.rb
@@ -11,6 +11,22 @@ class Game
 
   def start
     @board.create_board
+    #@board.show_board
+    @board.update_board(5,"X")
+    @board.show_board
+    turn
+  end
+
+  def turn
+    puts "What number will you place your marker?"
+    position = gets.chomp.to_i
+    
+    while @board.update_board(position,currentmarker) == false
+      puts "That spot is already taken"
+      position = gets.chomp.to_i
+    end
+    
+    puts "Marker Placed!"
     @board.show_board
   end
 end
--- main.rb
+++ main.rb
@@ -4,4 +4,3 @@ game = Game.new
 
 game.start
 
-
