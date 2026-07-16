49da7af05c69c21ed617473ce1912b05f576681a
Jeff <jmartinbaldwin@gmail.com>
2026-07-16 22:14:46 +0900

    complete tests for board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -23,9 +23,108 @@ RSpec.describe Board do
       expect(result).to eq(false)
       
     end
-
-
   end
+
+  describe '#winner?'do
+    it "returns true when same marker occupies 3 spaces in a row" do
+      board = Board.new
+      marker = "X"
+
+      position = 1
+      board.update_board(position, marker)
+
+      position = 2
+      board.update_board(position, marker)
+
+      position = 3
+      board.update_board(position, marker)
+
+      result = board.winner?(marker)
+
+      expect(result).to eq(true)
+    end
+
+    it "returns true when the same marker occupies 3 spaces in a column" do
+      board = Board.new
+      marker = "X"
+
+      position = 1
+      board.update_board(position, marker)
+
+      position = 4
+      board.update_board(position, marker)
+
+      position = 7
+      board.update_board(position, marker)
+
+      result = board.winner?(marker)
+
+      expect(result).to eq(true)
+    end
+
+    it "returns true when the same marker occupies 3 spaces in a diagonal" do
+      board = Board.new
+      marker = "X"
+
+      position = 1
+      board.update_board(position, marker)
+
+      position = 5
+      board.update_board(position, marker)
+
+      position = 9
+      board.update_board(position, marker)
+
+      result = board.winner?(marker)
+
+      expect(result).to eq(true)
+
+    end
+
+    it "returns false when the same marker does not occupy 3 spaces in a " do
+      board = Board.new
+      marker = "X"
+      result = board.winner?(marker)
+      expect(result).to eq(false)
+    end
+  end
+
+  describe '#draw?'do
+    it "returns true when the board has no integers left on it" do
+      board = Board.new
+      marker = "X"
+
+      position = 1
+      board.update_board(position, marker)
+      position = 2
+      board.update_board(position, marker)
+      position = 3
+      board.update_board(position, marker)
+      position = 4
+      board.update_board(position, marker)
+      position = 5
+      board.update_board(position, marker)
+      position = 6
+      board.update_board(position, marker)
+      position = 7
+      board.update_board(position, marker)
+      position = 8
+      board.update_board(position, marker)
+      position = 9
+      board.update_board(position, marker)
+
+      result = board.draw?
+      expect(result).to eq(true)
+    end
+
+    it "returns false when the board has any number of integers left on it" do
+      board = Board.new
+
+      result = board.draw?
+      expect(result).to eq(false)
+    end
+  end
+
 end
 
 
